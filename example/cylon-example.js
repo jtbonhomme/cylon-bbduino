c1135beadbd0f35bdee0f69416d6c5be686ea577
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2015-05-02 18:07:42 +0200

    added ping command

--- example/cylon-example.js
+++ example/cylon-example.js
@@ -20,6 +20,33 @@ Cylon.robot({
   },
 
   work: function(my) {
+
+    my.bbduino.on('connect', function() {
+      console.log('[cylon-bbduino] [work] connected');
+    });
+
+    my.bbduino.on('notification', function(data) {
+      console.log('[cylon-bbduino] [work] notification : ' + JSON.stringify(data));
+    });
+
+    my.bbduino.on('message', function(data) {
+      console.log('[cylon-bbduino] [work] message : ' + data);
+    });
+
+    my.bbduino.on('data', function(data) {
+      console.log('[cylon-bbduino] [work] data : ' + data);
+    });
+
+    my.sphero.on('disconnect', function() {
+      bbduino.log('[cylon-bbduino] [work] disconnect');
+    });
+
+    setTimeout(function(){
+      my.bbduino.ping(function() {
+        console.log('[cylon-bbduino] [work] Bbduino pinged');
+      });
+  }, 5000);
+
   }
 });
 
