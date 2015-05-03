813190c3fb4c85b7477f5cf16b5eb88ec0117eae
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2015-05-03 16:24:05 +0200

    Fixed callback undefined

--- example/cylon-example.js
+++ example/cylon-example.js
@@ -12,14 +12,41 @@ Cylon.robot({
   name: "MyBbduino",
 
   connections: {
+    keyboard: { adaptor: 'keyboard' },
     bbduino: { adaptor: 'bbduino', port: '/dev/cu.HC-05-DevB' }
   },
 
   devices: {
+    keyboard: { driver: 'keyboard' },
     bbduino: { driver: 'bbduino', connection: 'bbduino' }
   },
 
   work: function(my) {
+    var servoPos = 128;
+
+    my.keyboard.on("keydown", function(key) {
+      console.log(key.name + " keydown!");
+      if(key.name == 'a') {
+        my.bbduino.servo((servoPos<255)?servoPos+=10:255, function() {
+          console.log('[cylon-bbduino] [work] Bbduino servo pos : ' + servoPos);
+        });
+      }
+      else if(key.name == 'z') {
+        my.bbduino.servo((servoPos>0)?servoPos-=10:0, function() {
+          console.log('[cylon-bbduino] [work] Bbduino servo pos : ' + servoPos);
+        });
+      }
+      else if(key.name == 'up') {
+        my.bbduino.speed(200, 200);
+      }
+    });
+
+    my.keyboard.on("keyup", function(key) {
+      console.log(key.name + " keyup!");
+      if(key.name == 'up') {
+        my.bbduino.speed(0, 0);
+      }
+    });
 
     my.bbduino.on('connect', function() {
       console.log('[cylon-bbduino] [work] connected');
@@ -33,38 +60,34 @@ Cylon.robot({
       bbduino.log('[cylon-bbduino] [work] disconnect');
     });
 
-    my.bbduino.getVersion(function(version) {
-      console.log('[cylon-bbduino] [work] Bbduino version ' + version);
+    my.bbduino.servo(servoPos, function() {
+      console.log('[cylon-bbduino] [work] Bbduino servo pos : ' + servoPos);
     });
+    my.bbduino.ping(function() {
+      console.log('[cylon-bbduino] [work] Bbduino pinged');
+    });
+    // setTimeout(function(){
+    //   my.bbduino.servo(5, function() {
+    //     console.log('[cylon-bbduino] [work] Bbduino servo 5');
+    //   });
+    // }, 6500);
 
-    setTimeout(function(){
-      my.bbduino.ping(function() {
-        console.log('[cylon-bbduino] [work] Bbduino pinged');
-      });
-    }, 5000);
+    // setTimeout(function(){
+    //   my.bbduino.servo(250, function() {
+    //     console.log('[cylon-bbduino] [work] Bbduino servo 250');
+    //   });
+    // }, 8500);
 
-    setTimeout(function(){
-      my.bbduino.servo(5, function() {
-        console.log('[cylon-bbduino] [work] Bbduino servo 5');
-      });
-    }, 6500);
+    // setTimeout(function(){
+    //   my.bbduino.disconnect(function() {
+    //     console.log('[cylon-bbduino] [work] Bbduino disconnect');
+    //   });
+    // }, 10000);
 
-    setTimeout(function(){
-      my.bbduino.servo(250, function() {
-        console.log('[cylon-bbduino] [work] Bbduino servo 250');
-      });
-    }, 8500);
-
-    setTimeout(function(){
-      my.bbduino.disconnect(function() {
-        console.log('[cylon-bbduino] [work] Bbduino disconnect');
-      });
-    }, 10000);
-
-    setTimeout(function(){
-       console.log('Goodbye ...');
-       process.exit(0);
-    }, 15000);
+    // setTimeout(function(){
+    //    console.log('Goodbye ...');
+    //    process.exit(0);
+    // }, 15000);
   }
 });
 
